32a004a6855a9a61f9c9b63c93432a6f80cf4eb5
samyatsang412 <samyatsang412@gmail.com>
2026-07-02 13:59:26 +0800

    added 3rd number

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 function calcSum(){
     const num1=document.getElementById('num1').value;
     const num2=document.getElementById('num2').value;
+    const num3=document.getElementById('num3').value;
 
     const n1 = parseInt(num1);
     const n2 = parseInt(num1);
-    const sum = n1 + n2;
+    const n3 = parseInt(num1);
+    const sum = n1 + n2 + n3;
 
     //Display the result
     document.getElementById('answer').textContent = sum;
@@ -13,5 +15,6 @@ function calcSum(){
 function clearFields(){
     document.getElementById('num1').value ="";
     document.getElementById('num2').value ="";
+    document.getElementById('num3').value ="";
     document.getElementById('answer').textContent ="";
 }
